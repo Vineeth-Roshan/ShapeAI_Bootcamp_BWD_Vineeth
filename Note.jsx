3e78a0154de2aee9027dc413c2f7580ce594c24d
Vineeth-Roshan <85430303+Vineeth-Roshan@users.noreply.github.com>
2021-06-06 13:59:34 +0530

    Update Note.jsx

--- Note.jsx
+++ Note.jsx
@@ -5,8 +5,8 @@ function Note() {
     <div className="note">
       <h1>Javascript and React.js</h1>
       <p>
-        This was an amazing bootcamp taken up by Shaurya Sinha We covered
-        everything from Scratch including Javascript React.js, HTML.
+        This was an amazing bootcamp taken up by Shaurya Sinha.We covered
+        everything from Scratch including Javascript React.js, HTML.It was fun.
       </p>
     </div>
   );
